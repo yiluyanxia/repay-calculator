b31101785a427376e7119f541726c5c5ef56bf12
yiluyanxia <yiluyanxia@192.168.0.100>
2019-04-18 12:35:12 +0800

    add logo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div class="header">
-      计还器
+      <img class="logo" src="/static/images/logo.png" background-size="cover" />
+      <h3>计还器</h3>
     </div>
     <formItem label="借款金额" v-model="loan.loanNum" unit="元" placeholder="10000.00" @blur="handleNum"></formItem>
     <formItem label="借款期限" v-model="loan.loanTerm" unit="月" placeholder="1-360" @blur="handleTerm"></formItem>
@@ -33,12 +34,64 @@ export default {
 
   methods: {
     goDetail (type) {
-      // const url = '../detail/main'
-      // let url = '../detail/main?loan=' + JSON.stringify(this.loan)
+      if (this.loan.loanNum === '') {
+        wx.showToast({
+          icon: 'none',
+          title: '请输入金额',
+          duration: 2000,
+          mask: true
+        })
+        return
+      }
+      if (this.loan.loanTerm === '') {
+        wx.showToast({
+          icon: 'none',
+          title: '请输入期限',
+          duration: 2000,
+          mask: true
+        })
+        return
+      }
+      if (this.loan.loanRate === '') {
+        wx.showToast({
+          icon: 'none',
+          title: '请输入年利率',
+          duration: 2000,
+          mask: true
+        })
+        return
+      }
+      if (!this.loan.loanNum.match(/^([1-9]{0,1}[0-9]{0,6})(\.\d{1,2})?$/)) {
+        console.log('no')
+        wx.showToast({
+          icon: 'none',
+          title: '金额格式错误',
+          duration: 2000,
+          mask: true
+        })
+        return
+      }
+      if (!this.loan.loanTerm.match(/^([0-9]{1,2}|1[0-9]{2}|3[0-5][0-9]|360)$/)) {
+        console.log('no')
+        wx.showToast({
+          icon: 'none',
+          title: '期限格式错误',
+          duration: 2000,
+          mask: true
+        })
+        return
+      }
+      if (!this.loan.loanNum.match(/^([1-9]{0,1}[0-9]{0,6})(\.\d{1,2})?$/)) {
+        console.log('no')
+        wx.showToast({
+          icon: 'none',
+          title: '金额格式错误',
+          duration: 2000,
+          mask: true
+        })
+        return
+      }
       let url = '../detail/main?type=' + type + '&&' + 'loan=' + JSON.stringify(this.loan)
-      console.log('index is')
-      console.log(this.loan)
-      // console.log(this.motto)
       mpvue.navigateTo({url})
     },
     handleTerm () {
@@ -46,7 +99,7 @@ export default {
         console.log('no')
         wx.showToast({
           icon: 'none',
-          title: '期限只能为0-360',
+          title: '期限格式错误',
           duration: 2000,
           mask: true
         })
@@ -77,21 +130,32 @@ export default {
   },
 
   created () {
-    // let app = getApp()
   }
 }
 </script>
 
 <style scoped>
   .header {
-    height: 200rpx;
+    height: 300rpx;
     text-align: center;
-    line-height: 200rpx;
     background: #747EFF;
     background: -webkit-linear-gradient(left,#747EFF,#ABB5FF);
     background: linear-gradient(left,#747EFF,#ABB5FF);
     color: #fff;
     margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .header h3 {
+
+  }
+  .logo {
+    width: 128rpx;
+    height: 128rpx;
+    margin: 20rpx;
+    border-radius: 50%;
   }
   .btnbox {
     display: flex;
